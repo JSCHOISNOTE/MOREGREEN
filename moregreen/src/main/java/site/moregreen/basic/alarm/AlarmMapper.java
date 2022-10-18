e718d8b824e2496b5388c070fcfea450db2c9f65
USER <USER@192.168.24.48>
2022-10-18 15:52:28 +0900

    MORE GREEN 10/11 3rd project finish

---
+++ moregreen/src/main/java/site/moregreen/basic/alarm/AlarmMapper.java
@@ -0,0 +1,17 @@
+package site.moregreen.basic.alarm;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import site.moregreen.basic.command.AlarmDto;
+
+@Mapper
+public interface AlarmMapper {
+
+	public void saveAlarm(AlarmDto dto); 		//알람 저장
+	
+	public List<AlarmDto> selectAlarmList();	//알람 목록 조회
+	public void deleteAlarm(AlarmDto dto); 	//선택한 알림 삭제
+	
+}
---
+++ moregreen/src/main/java/site/moregreen/basic/alarm/AlarmService.java
@@ -0,0 +1,13 @@
+package site.moregreen.basic.alarm;
+
+import java.util.List;
+
+import site.moregreen.basic.command.AlarmDto;
+
+public interface AlarmService {
+
+	public int saveAlarm(AlarmDto dto);				//알람 저장
+		
+	public List<AlarmDto> retrieveAlarmList(); 	//알람 목록 가져오기
+	public void removeAlarm(AlarmDto dto); 	//선택한 알림 삭제
+}
